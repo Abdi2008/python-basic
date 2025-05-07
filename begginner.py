ed2d2a15191c0d7f47fa4424f0fee4240c108ee1
Abdirahman Abshir <abdirahmanabshir100@gmail.com>
2025-05-07 22:19:02 +0300

    7th

--- begginner.py
+++ begginner.py
@@ -21,12 +21,3 @@
 #full_name="Abdirahman "
 #last_name="Abshir"
 #print(full_name.title()+last_name.title())
-#import this #it gives you motivation you need to learn pyhton and how to use it correctly
-bicycles = ['trek', 'cannondale', 'redline', 'specialized'] 
-print(bicycles) #python returns all of the list but they have the bracket and strings
-print(bicycles[0]) #python naming convention starts from 0 not 1
-print(bicycles[1])
-print(bicycles[2])
-print(bicycles[3])
-print(bicycles[-1]) #negative one is used to capture the last item on the list this will come in handy when you don't know how long the list is
-print("This was my first bicycle ever: ",bicycles[0])
