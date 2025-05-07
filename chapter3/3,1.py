66140c96e698838a82337def3a2109fa1a83e367
Abdirahman Abshir <abdirahmanabshir100@gmail.com>
2025-05-07 20:49:58 +0300

    1

--- chapter3/3,1.py
+++ chapter3/3,1.py
@@ -7,14 +7,24 @@
 #print(bicycles[3])
 #print(bicycles[-1]) #negative one is used to capture the last item on the list this will come in handy when you don't know how long the list is
 #print("This was my first bicycle ever: ",bicycles[0]) #you can also use this on a print
-name=["abshir","luul","fowziyo","mohamed","abdirahman","abdullahi","harun","asma","zuhur"]
-message="Hy Welcome "
-print(message+name[0].title())
-print(message+name[1].title())
-print(message+name[2].title())
-print(message+name[3].title())
-print(message+name[4].title())
-print(message+name[-4].title())
-print(message+name[-3].title())
-print(message+name[-2].title())
-print(message+name[-1].title())
+#name=["abshir","luul","fowziyo","mohamed","abdirahman","abdullahi","harun","asma","zuhur"]
+#message="Hy Welcome "
+#print(message+name[0].title())
+#print(message+name[1].title())
+#print(message+name[2].title())
+#print(message+name[3].title())
+#print(message+name[4].title())
+#print(message+name[-4].title())
+#print(message+name[-3].title())
+#print(message+name[-2].title())
+#print(message+name[-1].title())
+#print("i like my mother who's name is ",name[1])
+nganya=["stung","stung2.0","redeemer","Givenchy","eschol","space x"]
+print("The best Nganya in Komarock is: ",nganya[1].title())
+nganya.append("Shady")          #Append is used to enter a new element in a list it automatically places the element last value of the list
+nganya.insert(2,"Edge")         #insert let's you choose whichever place you wanna put your element in the list.
+#del nganya[7]           #You use the del to remove an element from a list.
+#print(nganya)
+#popped=nganya.pop(1)    #When you use pop you delete the item from the list but you'll sitll have it. you'll use popped to get the value back
+#print(nganya)           #when you use pop alone it removes the last one but you can change the value in the brackets to change the value you want
+#print(popped)
