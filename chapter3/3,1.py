1368b1777016ab858f780df279815e1ef5ad445f
Abdirahman Abshir <abdirahmanabshir100@gmail.com>
2025-05-07 22:17:52 +0300

    finished the quiz finally

--- chapter3/3,1.py
+++ chapter3/3,1.py
@@ -19,12 +19,61 @@
 #print(message+name[-2].title())
 #print(message+name[-1].title())
 #print("i like my mother who's name is ",name[1])
-nganya=["stung","stung2.0","redeemer","Givenchy","eschol","space x"]
-print("The best Nganya in Komarock is: ",nganya[1].title())
-nganya.append("Shady")          #Append is used to enter a new element in a list it automatically places the element last value of the list
-nganya.insert(2,"Edge")         #insert let's you choose whichever place you wanna put your element in the list.
+#nganya=["stung","stung2.0","redeemer","givenchy","eschol","space x"]
+#print("The best Nganya in Komarock is: ",nganya[1].title())
+#nganya.append("Shady")          #Append is used to enter a new element in a list it automatically places the element last value of the list
+#nganya.insert(2,"Edge")         #insert let's you choose whichever place you wanna put your element in the list.
 #del nganya[7]           #You use the del to remove an element from a list.
 #print(nganya)
 #popped=nganya.pop(1)    #When you use pop you delete the item from the list but you'll sitll have it. you'll use popped to get the value back
 #print(nganya)           #when you use pop alone it removes the last one but you can change the value in the brackets to change the value you want
 #print(popped)
+#shit_car="givenchy"       #you can assign a variable for the element you wanna delete
+#nganya.remove("eschol")     #or you can insert the value straight in the brackets
+#print(nganya)
+        #excersies 
+    #3.4 GUEST LIST
+guests=["maryan","mahad","mohamed adan"]
+print("Hey ",guests[0]," you are invited to my wedding")
+print("Hey ",guests[1]," you are invited to my wedding")
+print("Hey ",guests[2]," you are invited to my wedding")
+    #a guest said he cant make it so i gotta change him with another person
+print("\t \t This guest can't make it: ",guests[2].title()," so i have to find a solution")
+del guests[2] #i have deleted the guy who can't make
+guests.insert(2,"nasrudin") # i replaced him with another guy using the insert method
+print("\t Here's my new updated list ",guests)
+print("Hey ",guests[0].title()," you are invited to my wedding")
+print("Hey ",guests[1].title()," you are invited to my wedding")        #Here are my new invations for the new list
+print("Hey ",guests[2].title()," you are invited to my wedding")
+    # 3-6. More Guests: I have to add more guests since the venue of the wedding is changed
+print("\t I have to add more guests since the venue of the wedding is changed")
+#        Use insert() to add one new guest to the beginning of your list.
+#       Use insert() to add one new guest to the middle of your list.
+#       Use append() to add one new guest to the end of your list
+guests.insert(0,"abdikani")
+guests.insert(2,"mudi")
+guests.append("ayub")
+print("You're invited to the wedding of Mr.Abdirahman Abshir Welcome Mr: ",guests[0].title())
+print("You're invited to the wedding of Mr.Abdirahman Abshir Welcome Md: ",guests[1].title())
+print("You're invited to the wedding of Mr.Abdirahman Abshir Welcome Mr: ",guests[2].title())
+print("You're invited to the wedding of Mr.Abdirahman Abshir Welcome Mr: ",guests[3].title())
+print("You're invited to the wedding of Mr.Abdirahman Abshir Welcome Mr: ",guests[4].title())
+print("You're invited to the wedding of Mr.Abdirahman Abshir Welcome Mr: ",guests[5].title())
+        #3-7. Shrinking Guest List:
+print("\t \t \t I CAN ONLY INVITE TWO PEOPLE")
+#remove guests from your list one at a time until only two names remain in your list. Each time you pop a name from your list, print a message to that person letting them know you’re sorry you can’t invite them to dinner.
+popped=guests.pop(-1)
+print("Hey ",popped," i'm sorry i can't invite you since my bride isn't comfortable with many people")
+popped=guests.pop(4)
+print("Hey ",popped," i'm sorry i can't invite you since my bride isn't comfortable with many people")
+popped=guests.pop(3)
+print("Hey ",popped," i'm sorry i can't invite you since my bride isn't comfortable with many people")
+popped=guests.pop(0)
+print("Hey ",popped," i'm sorry i can't invite you since my bride isn't comfortable with many people")
+#       remind the ones who are still invited
+print("Hey",guests[0].title()," you're still invited")
+print("Hey",guests[1].title()," you're still invited")
+        #Use del to remove the last two names from your list, so you have an empty list. Print your list to make sure you actually have an empty list at the end of your program
+del guests[0]
+del guests[0]
+print(guests,"POV the list is empty")
