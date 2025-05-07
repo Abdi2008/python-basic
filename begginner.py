9e7c20a7de9201dda23d75ea7cc3bf0a7365c3e8
Abdirahman Abshir <abdirahmanabshir100@gmail.com>
2025-05-07 18:27:25 +0300

    0pressure

--- begginner.py
+++ begginner.py
@@ -18,6 +18,15 @@
  #   print("Hi",names)
 #greet("Abdi")
     #def is used to define and give the function any name and open the brackets. After you finish the code you call the code by calling it its name then the (). e.g greet()
-full_name="Abdirahman "
-last_name="Abshir"
-print(full_name.title()+last_name.title())
+#full_name="Abdirahman "
+#last_name="Abshir"
+#print(full_name.title()+last_name.title())
+#import this #it gives you motivation you need to learn pyhton and how to use it correctly
+bicycles = ['trek', 'cannondale', 'redline', 'specialized'] 
+print(bicycles) #python returns all of the list but they have the bracket and strings
+print(bicycles[0]) #python naming convention starts from 0 not 1
+print(bicycles[1])
+print(bicycles[2])
+print(bicycles[3])
+print(bicycles[-1]) #negative one is used to capture the last item on the list this will come in handy when you don't know how long the list is
+print("This was my first bicycle ever: ",bicycles[0])
---
+++ chapter3/3,1.py
@@ -0,0 +1,20 @@
+#import this #it gives you motivation you need to learn pyhton and how to use it correctly
+#bicycles = ['trek', 'cannondale', 'redline', 'specialized'] 
+#print(bicycles) #python returns all of the list but they have the bracket and strings
+#print(bicycles[0]) #python naming convention starts from 0 not 1
+#print(bicycles[1])
+#print(bicycles[2])
+#print(bicycles[3])
+#print(bicycles[-1]) #negative one is used to capture the last item on the list this will come in handy when you don't know how long the list is
+#print("This was my first bicycle ever: ",bicycles[0]) #you can also use this on a print
+name=["abshir","luul","fowziyo","mohamed","abdirahman","abdullahi","harun","asma","zuhur"]
+message="Hy Welcome "
+print(message+name[0].title())
+print(message+name[1].title())
+print(message+name[2].title())
+print(message+name[3].title())
+print(message+name[4].title())
+print(message+name[-4].title())
+print(message+name[-3].title())
+print(message+name[-2].title())
+print(message+name[-1].title())
