db5e400358435f42085b770e38227cfa77067a3e
Abdirahman Abshir <abdirahmanabshir100@gmail.com>
2025-05-12 22:02:52 +0300

    12th

--- chapter3/3,2.py
+++ chapter3/3,2.py
@@ -1,3 +1,4 @@
 cars="posche","g-wagon","ferari","fortuner","hilux","alto","mercedes" # list of cars
 #cars.sorted()
+print("Total Number of cars: ",len(cars))
 print(sorted(cars))
--- quiz.py
+++ quiz.py
@@ -31,24 +31,57 @@
 
 
 #Excersie : Write a program that asks the user to enter the marks of 8 units and then calculate the total marks and average.
-greet=input("Are you ready to upload your marks ? (yes/no): ")
-if greet=="yes":
-    marks1=int(input("Enter your marks for Operating System: "))
-    marks2=int(input("Enter your marks for Computer Maths: "))
-    marks3=int(input("Enter your marks for Computer Maintenance: "))
-    marks4=int(input("Enter your marks for Java Programming: "))
-    marks5=int(input("Enter your marks for C++: "))
-    marks6=int(input("Enter your marks for Word Application: "))
-    marks7=int(input("Enter your marks for Computing project: "))
-    marks8=int(input("Enter your marks for Industrial Attachment: "))
-else: 
-    print("You are not ready to upload your marks")
-total_marks=marks1+marks2+marks3+marks4+marks5+marks6+marks7+marks8
-print("Your total marks are: " + str(total_marks))
-if total_marks >= 500:
-    print("Congratulations you have passed")
-elif total_marks >= 400:
-    print("Pull up your socks and work harder next time")
-elif total_marks <= 300:
-    print("You are a fraud and you should do better!! ")
-     
+#greet=input("Are you ready to upload your marks ? (yes/no): ")
+#if greet=="yes":
+ #   marks1=int(input("Enter your marks for Operating System: "))
+  #  marks2=int(input("Enter your marks for Computer Maths: "))
+   # marks3=int(input("Enter your marks for Computer Maintenance: "))
+    #marks4=int(input("Enter your marks for Java Programming: "))
+    #marks5=int(input("Enter your marks for C++: "))
+    #marks6=int(input("Enter your marks for Word Application: "))
+    #marks7=int(input("Enter your marks for Computing project: "))
+    #marks8=int(input("Enter your marks for Industrial Attachment: "))
+#else: 
+ #   print("You are not ready to upload your marks")
+#total_marks=marks1+marks2+marks3+marks4+marks5+marks6+marks7+marks8
+#print("Your total marks are: " + str(total_marks))
+#if total_marks >= 500:
+ #   print("Congratulations you have passed")
+#elif total_marks >= 400:
+#    print("Pull up your socks and work harder next time")
+#elif total_marks <= 300:
+ #   print("You are a fraud and you should do better!! ")
+#FizzBuzz:
+#Write a program that prints numbers from 1 to 100. For multiples of 3, print "Fizz"; for multiples of 5, print "Buzz"; for multiples of both, print "FizzBuzz".
+#for number in range(1,100):
+    #if number % 3== 0 and number % 5==0:
+     #   print(number," FizzBuzz!")
+    #elif number % 3 ==0:
+     #   print(number," Bizz!")                 #Order matters
+    #elif number % 5 == 0:
+     #   print(number," Fizz!")
+    #else:
+       # print(number)
+#Prime Checker:
+#Write a function to check if a number is prime. Return True or False.
+
+num=int(input("Enter a number to see if it's a prime number: "))
+if num % 1 == 0 and num % num == 0:
+    print("Prime Number")
+else:
+    print("Not Prime Number")
+
+
+
+
+
+
+
+
+#Ask a user to input a number and then check if the number is even , odd or prime number.
+#number=int(input("Enter a number to check if it's Even or Odd: "))
+#if number % 2==0:
+ #   print("Even")
+#else:
+ #   print("Odd")
+
