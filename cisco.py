e65381cd3ef0ad6e1f9c0e97942ab3eacfd583b4
Abdirahman Abshir <abdirahmanabshir100@gmail.com>
2025-10-19 21:46:37 +0300

    oct

--- cisco.py
+++ cisco.py
@@ -258,9 +258,11 @@
 #     n -= 1
 # else:
 #     print(n)
-n = range(4)
+# n = range(4)
  
-for num in n:
-    print(num - 1)
-else:
-    print(num)
+# for num in n:
+#     print(num - 1)
+# else:
+#     print(num)
+for i in range(0, 6, 3):
+    print(i)
