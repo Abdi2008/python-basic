dafe5ee024b6069f63c005ad36b8088ca2a406ba
Abdirahman Abshir <abdirahmanabshir100@gmail.com>
2025-10-20 21:43:04 +0300

    oct

--- cisco.py
+++ cisco.py
@@ -269,6 +269,45 @@
 # i = 1
 # j = not not i
 # print(i)
-numbers=[1,4,8,99,33,23,12]
-print(numbers)
-print("The total no of values in this variable is: ",len(numbers))
+# numbers=[1,4,8,99,33,23,12]
+# print(numbers)
+# print("The total no of values in this variable is: ",len(numbers))
+# hat=[1,2,3,4,5]
+# num1=int(input("Enter a number: "))
+# hat[2]=num1
+# del hat[-1]
+# print(len(hat))
+# print(hat)
+# numbers = [111, 7, 2, 1]
+# print(len(numbers))
+# print(numbers)
+
+# ###
+
+# numbers.append(4)
+
+# print(len(numbers))
+# print(numbers)
+
+# ###
+
+# numbers.insert(0, 222)
+# print(len(numbers))
+# print(numbers)
+
+# #
+# numbers.insert(1, 333)
+# print(numbers)
+# my_list = []  # Creating an empty list.
+ 
+# for i in range(5):
+#     my_list.insert(0, i + 1)
+ 
+# print(my_list)
+my_list = [10, 1, 8, 3, 5]
+total = 0
+ 
+for i in my_list:
+    total += i
+ 
+print(total)
