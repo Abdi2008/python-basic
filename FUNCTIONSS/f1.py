6a4c037cabd832bb7dc62e21d375efdb492ec5ae
Abdirahman Abshir <abdirahmanabshir100@gmail.com>
2025-12-25 17:17:43 +0300

    Learning Functionss

---
+++ FUNCTIONSS/f1.py
@@ -0,0 +1,14 @@
+def my_first():
+    print("\tEnter a Value: ")
+
+
+print("We start here .")
+my_first()
+a=int(input())
+my_first()
+b=int(input())
+my_first()
+c=int(input())
+sum=a+b+c
+print("\t The sum of all numbers is : "+ str(sum))
+print("We End Here.")
