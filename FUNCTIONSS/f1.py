fad2e42a9cd0cf58ec79c2d29c7673622349fb0c
Abdirahman Abshir <abdirahmanabshir100@gmail.com>
2025-12-26 11:23:29 +0300

    py

--- FUNCTIONSS/f1.py
+++ FUNCTIONSS/f1.py
@@ -1,14 +1,29 @@
-def my_first():
-    print("\tEnter a Value: ")
+# def my_first():
+#     print("\tEnter a Value: ")
 
 
-print("We start here .")
-my_first()
-a=int(input())
-my_first()
-b=int(input())
-my_first()
-c=int(input())
-sum=a+b+c
-print("\t The sum of all numbers is : "+ str(sum))
-print("We End Here.")
+# print("We start here .")
+# my_first()
+# a=int(input())
+# my_first()
+# b=int(input())
+# my_first()
+# c=int(input())
+# sum=a+b+c
+# print("\t The sum of all numbers is : "+ str(sum))
+# print("We End Here.")
+# def message(number):
+#     print("Enter a number:", number)
+
+# message(2)
+# def message(number):
+#     print("Enter a number:", number)
+
+# number = 1234
+# message(10)
+# print(number)
+def my_function(a, b, c):
+    print(a, b, c)
+ 
+my_function(1, 2, 3)
+
