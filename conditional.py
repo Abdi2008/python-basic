120ce0d5fc9f5f858de10fc3142f0e180bde8225
Abdirahman Abshir <abdirahmanabshir100@gmail.com>
2025-12-29 23:54:30 +0300

    Lists

---
+++ conditional.py
@@ -0,0 +1,8 @@
+weather='hot'
+if weather=='cold':
+    drink='Hot Coffee'
+    # print("Hey i'll like to drink "+ drink)
+else:
+    drink='Iced Coffee'
+    
+print("Hey i'll like to drink "+ drink)
---
+++ list.py
@@ -0,0 +1,17 @@
+def main():
+    #create a list of AWS Services
+    aws_services=['EC2', 'Lambda','Load Balancer', 'Elastic Bean Stalker','S3','VPC']
+    print(f"AWS services list:  {aws_services}")
+    #Access the last service in the list
+    print(f"The Last AWS service is :  {aws_services[-1]}")
+    #Modify the last service in the list
+    aws_services[-1]="Security Group"
+    #del(aws_services[-1])   # i deleted the last one 
+    print(aws_services)
+    #Add a new service to the list
+    aws_services.append('VPC')
+    print(aws_services)
+    #Remove the second service from the list
+    del(aws_services[1])
+    print(aws_services)
+main()
