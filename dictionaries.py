6a77fa073d82bc8bef65332bafb534ec6fdc1bb8
Abdirahman Abshir <abdirahmanabshir100@gmail.com>
2025-12-30 23:41:46 +0300

    dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,12 @@
+# #We gotta create a dictionary
+# salah={'Fajr': 2, 'Duhur' : 4, 'ASR' : 4}
+# print(salah)
+# # i want to add two other salahs
+# # salah['Maghrib','Isha']=3,4
+# salah['Maghrib']=3
+# salah['Isha']=4
+# print(salah)
+alien_0 = {'color': 'green'} 
+print(f"The alien is {alien_0['color']}.") 
+alien_0['color'] = 'yellow' 
+print(f"The alien is now {alien_0['color']}.")
