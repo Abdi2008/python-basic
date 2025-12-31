6318a4d896dc6f09b1e6a8e6a18e4f3acec223d1
Abdirahman Abshir <abdirahmanabshir100@gmail.com>
2025-12-31 22:18:25 +0300

    Bye 2025

--- dictionaries.py
+++ dictionaries.py
@@ -6,7 +6,14 @@
 # salah['Maghrib']=3
 # salah['Isha']=4
 # print(salah)
-alien_0 = {'color': 'green'} 
-print(f"The alien is {alien_0['color']}.") 
-alien_0['color'] = 'yellow' 
-print(f"The alien is now {alien_0['color']}.")
+# alien_0 = {'color': 'green'} 
+# print(f"The alien is {alien_0['color']}.") 
+# alien_0['color'] = 'yellow' 
+# print(f"The alien is now {alien_0['color']}.")
+favorite_languages = { 
+    'jen': 'python', 
+    'sarah': 'c', 
+    'edward': 'rust', 
+    'phil': 'python', 
+    }
+print(favorite_languages)
