790b7bc1b1c0aab40ca67ff096bfde0968be985a
Abdirahman Abshir <abdirahmanabshir100@gmail.com>
2026-01-04 12:49:54 +0300

    py

--- dictionaries.py
+++ dictionaries.py
@@ -10,10 +10,18 @@
 # print(f"The alien is {alien_0['color']}.") 
 # alien_0['color'] = 'yellow' 
 # print(f"The alien is now {alien_0['color']}.")
-favorite_languages = { 
-    'jen': 'python', 
-    'sarah': 'c', 
-    'edward': 'rust', 
-    'phil': 'python', 
-    }
-print(f"Sarah's Favourite Language is {favorite_languages['sarah'].title()}")
+# favorite_languages = { 
+#     'jen': 'python', 
+#     'sarah': 'c', 
+#     'edward': 'rust', 
+#     'phil': 'python', 
+#     }
+# print(f"Sarah's Favourite Language is {favorite_languages['sarah'].title()}")
+user_0 = { 
+    'username': 'efermi', 
+    'first': 'enrico', 
+    'last': 'fermi', 
+    } 
+for key, value in user_0.items(): 
+    print(f"\nKey: {key}") 
+    print(f"Value: {value}")
