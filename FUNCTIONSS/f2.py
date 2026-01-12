5ff37cd2c24202d0d8fdee94692a798d9acdbe9c
Abdirahman Abshir <abdirahmanabshir100@gmail.com>
2026-01-12 23:24:57 +0300

    pyy

--- FUNCTIONSS/f2.py
+++ FUNCTIONSS/f2.py
@@ -64,9 +64,14 @@
 #     print(f"This {animal_name} is {gender} is {species}")
 
 # vet("CROW","FEMALE","BIRD")
+# def describe_pet(animal_type, pet_name): 
+#     """Display information about a pet.""" 
+#     print(f"\nI have a {animal_type}.") 
+#     print(f"My {animal_type}'s name is {pet_name.title()}.") 
+# describe_pet('hamster', 'harry') 
+# describe_pet('dog', 'willie')
 def describe_pet(animal_type, pet_name): 
     """Display information about a pet.""" 
     print(f"\nI have a {animal_type}.") 
     print(f"My {animal_type}'s name is {pet_name.title()}.") 
-describe_pet('hamster', 'harry') 
-describe_pet('dog', 'willie')
+describe_pet('harry', 'hamster')
