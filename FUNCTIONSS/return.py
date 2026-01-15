e557de2c30c4a696e0a597bb32cf5727993922d4
Abdirahman Abshir <abdirahmanabshir100@gmail.com>
2026-01-15 23:53:22 +0300

    done

--- FUNCTIONSS/return.py
+++ FUNCTIONSS/return.py
@@ -10,21 +10,35 @@
 #     return person 
 # musician = build_person('jimi', 'hendrix') 
 # print(musician)
-def greeting(first_name, last_name, age, gender):
-    person={'firstName': first_name, 'LastName': last_name,'age': age, 'gender': gender }
-    return person
-while True:
-    print("Enter your Full details")
-    print("Enter q anytime to quite")
-    f_name=input("\tEnter your FirstName: ")
-    if f_name=="q":
-        break
-    l_name=input("\tEnter your SecondName: ")
-    if l_name=="q":
-        break
-    agee=input("\tEnter your Age: ")
-    genderr=input("\tEnter your Gender: ")
+# def greeting(first_name, last_name, age, gender):
+#     person={'firstName': first_name, 'LastName': last_name,'age': age, 'gender': gender }
+#     return person
+# while True:
+#     print("Enter your Full details")
+#     print("Enter q anytime to quite")
+#     f_name=input("\tEnter your FirstName: ")
+#     if f_name=="q":
+#         break
+#     l_name=input("\tEnter your SecondName: ")
+#     if l_name=="q":
+#         break
+#     agee=input("\tEnter your Age: ")
+#     genderr=input("\tEnter your Gender: ")
    
 
-    persona=greeting(f_name, l_name, agee, genderr)
-    print(f"Hello my names are {persona}")
+#     persona=greeting(f_name, l_name, agee, genderr)
+#     print(f"Hello my names are {persona}")
+def city_country(city_name,country):
+    furr_name={'cityName': city_name, 'CountryName': country}
+    return furr_name
+while True:
+    print("\tPress q if you want to quit")
+    city_name=input("Enter City Name: ")
+    if city_name=="q":
+        break
+    country=input("Enter Country Name: ")
+    if country=="q":
+        break
+    full_name=city_country(city_name,country)
+    print(full_name)
+
